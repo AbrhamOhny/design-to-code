ea9bd77e3eccf6fa7220624fb82869ce380f375f
AbrhamOhny <abrham.ohny@gmail.com>
2026-05-05 09:27:34 +0700

    Added contents of transactions

--- dashboard.bank-dash/resources/js/components/MiniTransactions.vue
+++ dashboard.bank-dash/resources/js/components/MiniTransactions.vue
@@ -1,29 +1,88 @@
 <script lang="ts" setup>
 import { computed } from "vue";
+import { getCurrentInstance } from "vue";
 
-const { amount, ttype, date, sender, receiver } = defineProps<{
+const { appContext } = getCurrentInstance()!;
+const capitalize = appContext.config.globalProperties.$capitalize;
+const { amount, date, sender, receiver } = defineProps<{
     amount: number;
-    ttype: string;
     date: Date;
     sender: string;
     receiver: string;
 }>();
 
+const getTransactionType = computed(() => {
+    if (sender === "user" || receiver === "user") return "transfer";
+    else if (sender === "self") return "withdraw";
+    else if (receiver === "self") return "deposit";
+    return "unknown";
+});
+
 const iconName = computed(() => {
-    switch (ttype) {
-        case "deposit":
-            return "vaadin:money-deposit";
+    switch (getTransactionType.value) {
         case "withdraw":
             return "vaadin:money-withdraw";
-        default: "mdi:question-mark";
+        case "deposit":
+            return "vaadin:money-deposit";
+        case "transfer":
+            return "mdi:instant-transfer";
+        default:
+            return "mdi:question-mark";
+    }
+});
+
+const getDescription = computed(() => {
+    return `${capitalize(getTransactionType.value)} from ${sender === "self" ? "my Card" : capitalize(sender)}`;
+});
+
+const isIncome = computed(() => {
+    switch (getTransactionType.value) {
+        case "deposit":
+            return 1;
+        case "withdraw":
+            return 0;
+        case "transfer":
+            return sender === "self" ? 0 : 1;
+        default:
+            return -1;
     }
 });
 
 const iconColor = computed(() => {
-    switch(ttype) {
-        case ""
+    const incomingTrans = ["var(--color-lime-800)", "var(--color-lime-300)"];
+    const outgoingTrans = ["var(--color-amber-800)", "var(--color-amber-300)"];
+    switch (isIncome.value) {
+        case 1:
+            return incomingTrans;
+        case 0:
+            return outgoingTrans;
+        default:
+            return ["var(--color-rose-800)", "var(--color-rose-950)"];
     }
-})
+});
 </script>
-
-<template></template>
+<template>
+    <div class="flex flex-row items-center gap-5">
+        <div
+            class="flex items-center justify-center p-4 rounded-full overflow-clip text-background"
+            :style="`background-color: ${iconColor[0]}; color: ${iconColor[1]}`"
+        >
+            <Icon :icon="iconName" width="24" height="24" />
+        </div>
+        <div class="flex flex-2 flex-col gap-2">
+            <span>{{ getDescription }}</span>
+            <span class="text-sm text-primary2-darker">{{
+                date.toLocaleDateString("en-GB", {
+                    day: "2-digit",
+                    month: "long",
+                    year: "numeric",
+                })
+            }}</span>
+        </div>
+        <div class="flex flex-1 justify-end">
+            <span :class="`${isIncome === 1 ? 'text-income' : 'text-outcome'}`">{{
+                `${isIncome === 1 ? "+" : "-"} $${amount}`
+            }}</span>
+        </div>
+    </div>
+</template>
--- dashboard.bank-dash/resources/js/pages/dashboard/HomeView.vue
+++ dashboard.bank-dash/resources/js/pages/dashboard/HomeView.vue
@@ -1,20 +1,42 @@
 <script lang="ts" setup>
 import CreditCard from "../../components/CreditCard.vue";
+import MiniTransactions from "../../components/MiniTransactions.vue";
 const name = "Test";
 const balance = 1500;
 const valid = new Date("2030-12");
 const uniqueID = 3778123412341234;
+
+const latestTransactions = [
+    {
+        amount: 850,
+        date: new Date("2021-1-28"),
+        sender: "self",
+        receiver: "cash",
+    },
+    {
+        amount: 2500,
+        date: new Date("2021-1-25"),
+        sender: "paypal",
+        receiver: "self",
+    },
+    {
+        amount: 5400,
+        date: new Date("2021-1-21"),
+        sender: "user",
+        receiver: "self",
+    },
+];
 </script>
 <template>
     <div class="flex flex-col gap-5">
         <!-- Section 1: Cards & Recent transaction -->
-        <div class="flex flex-row w-full gap-5">
+        <div class="flex flex-col md:flex-row w-full gap-5 overflow-x-auto">
             <div class="flex flex-col gap-5 flex-1/2">
                 <div class="flex flex-row items-center justify-between text-lg font-semibold">
                     <h1 class="text-lg font-semibold">My Cards</h1>
                     <span>See All</span>
                 </div>
-                <div class="flex flex-row gap-5">
+                <div class="flex flex-row gap-5 overflow-x-auto">
                     <CreditCard
                         :name="name"
                         :balance="balance"
@@ -35,7 +57,16 @@ const uniqueID = 3778123412341234;
                     <h1>Recent Transaction</h1>
                 </div>
 
-                <div class="flex flex-col gap-5 p-5 rounded-2xl bg-background-lighter"></div>
+                <div class="flex flex-col gap-5 p-5 rounded-2xl bg-background-lighter">
+                    <MiniTransactions
+                        v-for="(item, index) in latestTransactions"
+                        :key="index"
+                        :amount="item.amount"
+                        :date="item.date"
+                        :sender="item.sender"
+                        :receiver="item.receiver"
+                    />
+                </div>
             </div>
         </div>
     </div>
